0812e9b98fd63be13db861345b190f36cbffc8db
Abu Baker Iqbal <ahmil2010@gmail.com>
2023-06-07 15:42:32 +0100

    WIP: left side of home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,7 +5,22 @@ const Home: React.FC = () => {
   return (
     <div className="home">
       <div className="home__content">
-        <h1 className="home__text">Marketing that gets results</h1>
+        <div className="home__title">
+          <h1>Marketing that gets results</h1>
+        </div>
+        <div className="home__text">
+          <p>Supercharge your business growth with TikTok Ads!</p>
+          <p>Say goodbye to wasted time and ineffective ad campaigns</p>
+          <p>
+            Breakthrough the noise, skyrocket your sales, and unlock the full
+            potential of TikTok
+          </p>
+        </div>
+        <div className="home__button">
+          <button>
+            <span>Contact us now and prepare for remarkable results</span>
+          </button>
+        </div>
       </div>
     </div>
   );
