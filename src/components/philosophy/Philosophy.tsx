0a62db4d34fc3fb57a8ca00840f83308a6df0928
Abu Baker Iqbal <ahmil2010@gmail.com>
2023-06-14 21:05:09 +0100

    WIP: adding 4 boxes in a 2x2 formation.

--- src/components/philosophy/Philosophy.tsx
+++ src/components/philosophy/Philosophy.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import "./philosophy.css";
 
 const Philosophy: React.FC = () => {
   return (
-<></>
+    <div className="philosophy">
+      <div className="stats-text">
+        <p>Don't just take our word for it</p>
+      </div>
+      <div className="box-container container">
+        <div className="box box1">
+          <p>Box 1 content</p>
+        </div>
+        <div className="box box2">
+          <p>Box 2 content</p>
+        </div>
+        <div className="box box3">
+          <p>Box 3 content</p>
+        </div>
+        <div className="box box4">
+          <p>Box 4 content</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
